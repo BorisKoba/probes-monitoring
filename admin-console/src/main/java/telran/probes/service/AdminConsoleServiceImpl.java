bb0f838d30ea31957eafd67cce14b564d12fb083
BorisKoba <browi@mail.ru>
2024-03-15 09:39:48 +0300

    tacking from master code for cloud stream messaging

--- admin-console/src/main/java/telran/probes/service/AdminConsoleServiceImpl.java
+++ admin-console/src/main/java/telran/probes/service/AdminConsoleServiceImpl.java
@@ -23,10 +23,11 @@ import telran.probes.model.SensorRangeDoc;
 @RequiredArgsConstructor
 public class AdminConsoleServiceImpl implements AdminConsoleService {
 final MongoTemplate mongoTemplate;
-
+final StreamBridge streamBridge;
 String collectionNameRanges = "sensor_ranges";
 String collectionNameMails = "sensor_emails";
-
+@Value("${app.update.data.binding.name}")
+String bindingName;
 FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert(false);
 	@Override
 	public SensorRange addSensorRange(SensorRange sensorRange) {
@@ -71,7 +72,9 @@ FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert
 			throw new SensorNotFoundException(sensorId, collectionNameRanges);
 		}
 		log.debug("new range for sensor {} is {}", sensorId, range);
-		
+		SensorUpdateData updateData = new SensorUpdateData(sensorId, range, null);
+		streamBridge.send(bindingName, updateData);
+		log.debug("update data {} have been sent to binding name {}", updateData, bindingName);
 		return sensorRange;
 	}
 
@@ -88,7 +91,9 @@ FindAndModifyOptions options = new FindAndModifyOptions().returnNew(true).upsert
 			throw new SensorNotFoundException(sensorId, collectionNameMails);
 		}
 		log.debug("new remails for sensor {} is {}", sensorId, emails);
-		
+		SensorUpdateData updateData = new SensorUpdateData(sensorId, null, emails);
+		streamBridge.send(bindingName, updateData);
+		log.debug("update data {} have been sent to binding name {}", updateData, bindingName);
 		return sensorEmails;
 	}
 
